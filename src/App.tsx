b9e9a615f51c404bbf8a6b00a1e22a8aa9d9076a
axel6903 <axel.faou@gmail.com>
2023-11-16 18:22:40 +0100

    cherche les musiques

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -8,10 +9,15 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
+
+
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0)
+const goToNextTrack = () => {
+  setTrackIndex(trackIndex + 1);
+}
   return (
     <div className="App">
-      <audio src={trackUrls[2]} autoPlay controls />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Bienvenue sur le blind test</h1>
@@ -19,9 +25,15 @@ const App = () => {
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+      <button onClick={goToNextTrack}>
+        Next track
+      </button>
       <div className="App-buttons"></div>
     </div>
+
   );
+  
 };
 
 export default App;
